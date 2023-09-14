1d42b7a655c9e1bc4e37a61e9cb1a90ccd95a054
DenysShchypt <denysshchyptsovit@gmail.com>
2023-09-14 12:32:07 +0300

    createAPI

--- src/index.js
+++ src/index.js
@@ -1,1 +1,46 @@
 import 'modern-normalize';
+
+import { searchEvents, searchCategories, searchRecipesFilter, searchRecipesPopular, searchRecipesId, searchRecipesIdRating, searchIngredients, searchAreas, searchAddOrders } from './js/createAPI';
+
+// Прослуховувачі подій
+
+
+
+
+// Function для обробки промисів
+
+searchEvents().then(res => {
+    console.log(res);
+}).catch(error => { console.log(error) });
+
+searchCategories().then(res => {
+    console.log(res);
+}).catch(error => { console.log(error) });
+
+searchRecipesFilter().then(res => {
+    console.log(res);
+}).catch(error => { console.log(error) });
+
+searchRecipesPopular().then(res => {
+    console.log(res);
+}).catch(error => { console.log(error) });
+
+// searchRecipesId().then(res => {
+//     console.log(res);
+// }).catch(error => { console.log(error) });
+
+// searchRecipesIdRating().then(res => {
+//     console.log(res);
+// }).catch(error => { console.log(error) });
+
+searchIngredients().then(res => {
+    console.log(res);
+}).catch(error => { console.log(error) });
+
+searchAreas().then(res => {
+    console.log(res);
+}).catch(error => { console.log(error) });
+
+// searchAddOrders().then(res => {
+//     console.log(res);
+// }).catch(error => { console.log(error) });
