252c7da1af9676e4bdc510c27c11a35c59507fe0
batalova-kira <batalovakira@gmail.com>
2023-09-15 21:30:30 +0300

    update-markup-15-09-4

--- src/js/markup-card.js
+++ src/js/markup-card.js
@@ -22,15 +22,15 @@ const searchRecipesFilter = async (category,page,limit,time,area,ingredient) => 
         // Праметри API запиту
     const params = new URLSearchParams({
         
-        limit: 6,
+        limit: 9,
         
     });
         const response = await axios.get(`${URL}${resource.recipes}?${params}`);
         return response;
 };
 window.addEventListener('load', handler);
-function handler() { searchRecipesFilter().then(create).catch(error => { console.log(error) }) };
-function create(res) {  
+function handler() { searchRecipesFilter().then(renderMarkup).catch(error => { console.log(error) }) };
+function renderMarkup(res) {  
   elem.innerHTML = createMarkup(res.data.results);
     console.log(res);
 }
@@ -44,9 +44,9 @@ function createMarkup(params) {
         <div class="photo-thumb">
           <img class="photo-img" src="${elem.thumb}" alt="${elem.title}" loading="lazy"/>
         </div>
-        <button type="button" class="btn-favorite">
-                <svg class="icon-favorite" width="22" height="22">
-                  <use href="../images/icons.svg#heart"></use>
+        <button type="button" class="btn-favorite" >
+                <svg class="icon-favorite" width="22" height="22" viewBox="0 0 32 32">
+<path fill="none" opacity="0.5" stroke="#f8f8f8" stroke-linejoin="round" stroke-linecap="round" stroke-miterlimit="4" stroke-width="2.9091" d="M15.992 6.848c-2.666-3.117-7.111-3.955-10.451-1.101s-3.81 7.625-1.187 11c2.181 2.806 8.781 8.725 10.944 10.641 0.242 0.214 0.363 0.321 0.504 0.364 0.123 0.037 0.258 0.037 0.381 0 0.141-0.042 0.262-0.149 0.504-0.364 2.163-1.916 8.763-7.834 10.944-10.641 2.623-3.375 2.21-8.177-1.187-11.001s-7.785-2.015-10.451 1.101z"></path>
                 </svg>
         </button>
         <div class="info">
@@ -57,10 +57,22 @@ function createMarkup(params) {
                 ${elem.description}
             </p>
             <div class="info-bottom">
-            <div class="info-rating">
-            ${elem.rating}
-                <svg width="22" height="22">
-                  <use href="images/icons.svg#star"></use>
+            <div class="info-div">
+            <p class="info-rating">${elem.rating}</p>
+                <svg class="info-star" width="14" height="14" viewBox="0 0 32 32">                
+<path fill="#EEA10C" d="M13.826 3.262c0.684-2.106 3.663-2.106 4.348 0l1.932 5.945c0.306 0.942 1.184 1.579 2.174 1.579h6.251c2.214 0 3.135 2.833 1.344 4.135l-5.057 3.674c-0.801 0.582-1.136 1.614-0.83 2.556l1.931 5.945c0.684 2.106-1.726 3.857-3.517 2.555l-5.057-3.674c-0.801-0.582-1.886-0.582-2.687 0l-5.057 3.674c-1.791 1.302-4.202-0.45-3.517-2.555l1.932-5.945c0.306-0.942-0.029-1.973-0.83-2.556l-5.057-3.674c-1.791-1.302-0.871-4.135 1.344-4.135h6.251c0.99 0 1.868-0.638 2.174-1.579l1.932-5.945z"></path>
+                </svg>
+                <svg class="info-star" width="14" height="14" viewBox="0 0 32 32">                
+<path fill="#f8f8f8" opacity="0.3" d="M13.826 3.262c0.684-2.106 3.663-2.106 4.348 0l1.932 5.945c0.306 0.942 1.184 1.579 2.174 1.579h6.251c2.214 0 3.135 2.833 1.344 4.135l-5.057 3.674c-0.801 0.582-1.136 1.614-0.83 2.556l1.931 5.945c0.684 2.106-1.726 3.857-3.517 2.555l-5.057-3.674c-0.801-0.582-1.886-0.582-2.687 0l-5.057 3.674c-1.791 1.302-4.202-0.45-3.517-2.555l1.932-5.945c0.306-0.942-0.029-1.973-0.83-2.556l-5.057-3.674c-1.791-1.302-0.871-4.135 1.344-4.135h6.251c0.99 0 1.868-0.638 2.174-1.579l1.932-5.945z"></path>
+                </svg>
+                <svg class="info-star" width="14" height="14" viewBox="0 0 32 32">                
+<path fill="#f8f8f8" opacity="0.3" d="M13.826 3.262c0.684-2.106 3.663-2.106 4.348 0l1.932 5.945c0.306 0.942 1.184 1.579 2.174 1.579h6.251c2.214 0 3.135 2.833 1.344 4.135l-5.057 3.674c-0.801 0.582-1.136 1.614-0.83 2.556l1.931 5.945c0.684 2.106-1.726 3.857-3.517 2.555l-5.057-3.674c-0.801-0.582-1.886-0.582-2.687 0l-5.057 3.674c-1.791 1.302-4.202-0.45-3.517-2.555l1.932-5.945c0.306-0.942-0.029-1.973-0.83-2.556l-5.057-3.674c-1.791-1.302-0.871-4.135 1.344-4.135h6.251c0.99 0 1.868-0.638 2.174-1.579l1.932-5.945z"></path>
+                </svg>
+                <svg class="info-star" width="14" height="14" viewBox="0 0 32 32">                
+<path fill="#f8f8f8" opacity="0.3" d="M13.826 3.262c0.684-2.106 3.663-2.106 4.348 0l1.932 5.945c0.306 0.942 1.184 1.579 2.174 1.579h6.251c2.214 0 3.135 2.833 1.344 4.135l-5.057 3.674c-0.801 0.582-1.136 1.614-0.83 2.556l1.931 5.945c0.684 2.106-1.726 3.857-3.517 2.555l-5.057-3.674c-0.801-0.582-1.886-0.582-2.687 0l-5.057 3.674c-1.791 1.302-4.202-0.45-3.517-2.555l1.932-5.945c0.306-0.942-0.029-1.973-0.83-2.556l-5.057-3.674c-1.791-1.302-0.871-4.135 1.344-4.135h6.251c0.99 0 1.868-0.638 2.174-1.579l1.932-5.945z"></path>
+                </svg>
+                <svg class="info-star" width="14" height="14" viewBox="0 0 32 32">                
+<path fill="#f8f8f8" opacity="0.3" d="M13.826 3.262c0.684-2.106 3.663-2.106 4.348 0l1.932 5.945c0.306 0.942 1.184 1.579 2.174 1.579h6.251c2.214 0 3.135 2.833 1.344 4.135l-5.057 3.674c-0.801 0.582-1.136 1.614-0.83 2.556l1.931 5.945c0.684 2.106-1.726 3.857-3.517 2.555l-5.057-3.674c-0.801-0.582-1.886-0.582-2.687 0l-5.057 3.674c-1.791 1.302-4.202-0.45-3.517-2.555l1.932-5.945c0.306-0.942-0.029-1.973-0.83-2.556l-5.057-3.674c-1.791-1.302-0.871-4.135 1.344-4.135h6.251c0.99 0 1.868-0.638 2.174-1.579l1.932-5.945z"></path>
                 </svg>
             </div>
               <button class="info-btn">
@@ -74,6 +86,4 @@ function createMarkup(params) {
   }).join("");
 }
 
-
-
-export { create };
+export { renderMarkup };
