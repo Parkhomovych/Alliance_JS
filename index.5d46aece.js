9fe60f6e591fe9790d798eaf389bd6a5f4a34282
Parkhomovych <129387212+Parkhomovych@users.noreply.github.com>
2023-09-15 19:56:29 +0000

    Deploying to gh-pages from @ Parkhomovych/Alliance_JS@6afb648873fcde3c5398607ab5d22d98e54642ee 🚀

--- index.5d46aece.js
+++
@@ -1,2 +0,0 @@
-const e=document.querySelector(".check"),t=document.querySelector(".header"),c="light-theme",s="dark-theme";e.addEventListener("change",(function(e){e.currentTarget.checked?(t.classList.add(s),t.classList.remove(c)):(t.classList.add(c),t.classList.remove(s))}));
-//# sourceMappingURL=index.5d46aece.js.map
--- index.f34bbffb.js
+++
@@ -1,2 +0,0 @@
-!function(){var e=document.querySelector(".check"),t=document.querySelector(".header"),c="light-theme",r="dark-theme";e.addEventListener("change",(function(e){e.currentTarget.checked?(t.classList.add(r),t.classList.remove(c)):(t.classList.add(c),t.classList.remove(r))}))}();
-//# sourceMappingURL=index.f34bbffb.js.map
