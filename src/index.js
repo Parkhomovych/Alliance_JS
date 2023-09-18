58cd4a654beb061ff237312b073b4cb32a3016c5
Vladyslav Parkhomovych <adamantinevlad@gmail.com>
2023-09-19 01:56:44 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -6,6 +6,6 @@ import './js/localStorageJSON';
 import './js/hearts/cardHearts';
 import './js/hearts/heartsFill';
 import './js/categories';
-import './js/markup-card';
 import './js/hero';
 import './js/swiper';
+import './js/pagination'
