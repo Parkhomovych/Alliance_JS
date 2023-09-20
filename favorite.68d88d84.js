433d984fce6ca633521869ec6eb194144abfa02d
Parkhomovych <129387212+Parkhomovych@users.noreply.github.com>
2023-09-20 10:27:26 +0000

    Deploying to gh-pages from @ Parkhomovych/Alliance_JS@fca229758cf4758fb99d7cf35cc99066d97b273d 🚀

---
+++ favorite.68d88d84.js
@@ -0,0 +1,3 @@
+//# sourceMappingURL=favorite.68d88d84.js.map
+
+//# sourceMappingURL=favorite.68d88d84.js.map
---
+++ favorite.c854033c.js
@@ -0,0 +1,3 @@
+//# sourceMappingURL=favorite.c854033c.js.map
+
+//# sourceMappingURL=favorite.c854033c.js.map
