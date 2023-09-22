298c4c88d82038607cb0fdfce89027d1123266fd
Vladyslav Parkhomovych <adamantinevlad@gmail.com>
2023-09-22 17:14:57 +0300

    Update scrollToTop.js

--- src/js/scrollToTop.js
+++ src/js/scrollToTop.js
@@ -4,7 +4,9 @@ window.addEventListener('scroll', toggleButtonVisibility);
 function toggleButtonVisibility() {
   if (window.pageYOffset <= 200 && window.pageYOffset >= 0) {
     scrollButton.style.opacity = '0';
+    scrollButton.style.pointerEvents = 'none';
   } else {
     scrollButton.style.opacity = '1';
+    scrollButton.style.pointerEvents = 'all';
   }
-};
+}
